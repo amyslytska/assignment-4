783a4d680b87ffdca1158a50d98fdfe83be89387
Anna Myslytska <amyslytska@kse.org.ua>
2023-03-27 11:23:30 +0300

    Initial commit

---
+++ HuffmanCoding.cs
@@ -0,0 +1,20 @@
+﻿namespace assignment4;
+
+public class HuffmanCoding
+{
+    // 1) Calculate the frequency of each character in the string (dictionary).
+    
+    // 2) Sort the characters in increasing order of the frequency. Using priority queue
+    // or MIN HEAP.
+    
+    // 3) Make each unique character as a leaf node.
+    // Create an empty node z. Assign the minimum frequency to the left child of
+    // z and assign the second minimum frequency to the right child of z.
+    // Set the value of the z as the sum of the above two minimum frequencies.
+    
+    // 4) Remove these two minimum frequencies from Q and add the sum into the list of frequencies.
+    // 5) Insert node z into the tree.
+    // 6) Repeat steps 3 to 5 for all the characters.
+    // 7) For each non-leaf node, assign 0 to the left edge and 1 to the right edge.
+
+}
